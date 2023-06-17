df4e1172d6c2e7d7b3a7d08edf58fc9fa04a7e49
jepson <jiangpeng@itcast.cn>
2023-06-17 23:21:24 +0800

    底部导航链接

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,6 +1,9 @@
 // 按需导入
 import Vue from 'vue'
-import { Button, Switch, Rate } from 'vant'
+import { Button, Switch, Rate, Tabbar, TabbarItem } from 'vant'
+
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
 Vue.use(Rate)
 Vue.use(Button)
 Vue.use(Switch)
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>我是layout</div>
+  <div>
+    <!-- 二级路由出口：二级组件展示的位置 -->
+    <van-tabbar active-color="#ee0a24" inactive-color="#000">
+      <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="apps-o">分类页</van-tabbar-item>
+      <van-tabbar-item icon="shopping-cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
