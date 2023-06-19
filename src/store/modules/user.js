d22a02b0bfd2939c19a8d4946f81008a42ec6df3
jepson <jiangpeng@itcast.cn>
2023-06-19 19:07:35 +0800

    vuex持久化处理

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,18 +1,18 @@
+import { getInfo, setInfo } from '@/utils/storage'
+
 export default {
   namespaced: true,
   state () {
     return {
       // 个人权证相关
-      userInfo: {
-        token: '',
-        userId: ''
-      }
+      userInfo: getInfo()
     }
   },
   mutations: {
     // 所有mutations的第一个参数，都是state
     setUserInfo (state, obj) {
       state.userInfo = obj
+      setInfo(obj)
     }
   },
   actions: {},
---
+++ src/utils/storage.js
@@ -0,0 +1,19 @@
+// 约定一个通用的键名
+const INFO_KEY = 'hm_shopping_info'
+
+// 获取个人信息
+export const getInfo = () => {
+  const defaultObj = { token: '', userId: '' }
+  const result = localStorage.getItem(INFO_KEY)
+  return result ? JSON.parse(result) : defaultObj
+}
+
+// 设置个人信息
+export const setInfo = (obj) => {
+  localStorage.setItem(INFO_KEY, JSON.stringify(obj))
+}
+
+// 移除个人信息
+export const removeInfo = () => {
+  localStorage.removeItem(INFO_KEY)
+}
