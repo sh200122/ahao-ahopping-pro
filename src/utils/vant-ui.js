11ecfb751733ada5b9a6b923025164d67b207647
jepson <jiangpeng@itcast.cn>
2023-06-24 10:42:13 +0800

    首页动态渲染

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,7 +1,12 @@
 // 按需导入
 import Vue from 'vue'
-import { Toast, Button, Switch, Rate, Tabbar, TabbarItem, NavBar } from 'vant'
+import { Search, Swipe, SwipeItem, Grid, GridItem, Toast, Button, Switch, Rate, Tabbar, TabbarItem, NavBar } from 'vant'
 
+Vue.use(Search)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Grid)
+Vue.use(GridItem)
 Vue.use(Toast)
 Vue.use(NavBar)
 Vue.use(Tabbar)
