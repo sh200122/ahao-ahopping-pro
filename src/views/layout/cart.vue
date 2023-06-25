0a59be4941cba3e61887d207e1d60e83e36db31b
jepson <jiangpeng@itcast.cn>
2023-06-25 23:44:46 +0800

    空购物车处理

--- src/views/layout/cart.vue
+++ src/views/layout/cart.vue
@@ -1,48 +1,60 @@
 <template>
   <div class="cart">
     <van-nav-bar title="购物车" fixed />
-    <!-- 购物车开头 -->
-    <div class="cart-title">
-      <span class="all">共<i>{{ cartTotal }}</i>件商品</span>
-      <span class="edit" @click="isEdit = !isEdit">
-        <van-icon name="edit" />
-        编辑
-      </span>
-    </div>
 
-    <!-- 购物车列表 -->
-    <div class="cart-list">
-      <div class="cart-item" v-for="item in cartList" :key="item.goods_id">
-        <van-checkbox @click="toggleCheck(item.goods_id)"  :value="item.isChecked"></van-checkbox>
-        <div class="show">
-          <img :src="item.goods.goods_image" alt="">
+    <div v-if="isLogin && cartList.length > 0">
+      <!-- 购物车开头 -->
+      <div class="cart-title">
+        <span class="all">共<i>{{ cartTotal }}</i>件商品</span>
+        <span class="edit" @click="isEdit = !isEdit">
+          <van-icon name="edit" />
+          编辑
+        </span>
+      </div>
+
+      <!-- 购物车列表 -->
+      <div class="cart-list">
+        <div class="cart-item" v-for="item in cartList" :key="item.goods_id">
+          <van-checkbox @click="toggleCheck(item.goods_id)"  :value="item.isChecked"></van-checkbox>
+          <div class="show">
+            <img :src="item.goods.goods_image" alt="">
+          </div>
+          <div class="info">
+            <span class="tit text-ellipsis-2">{{ item.goods.goods_name }}</span>
+            <span class="bottom">
+              <div class="price">¥ <span>{{ item.goods.goods_price_min }}</span></div>
+              <!-- 既希望保留原本的形参，又需要通过调用函数传参 => 箭头函数包装一层 -->
+              <CountBox @input="(value) => changeCount(value, item.goods_id, item.goods_sku_id)" :value="item.goods_num"></CountBox>
+            </span>
+          </div>
         </div>
-        <div class="info">
-          <span class="tit text-ellipsis-2">{{ item.goods.goods_name }}</span>
-          <span class="bottom">
-            <div class="price">¥ <span>{{ item.goods.goods_price_min }}</span></div>
-            <!-- 既希望保留原本的形参，又需要通过调用函数传参 => 箭头函数包装一层 -->
-            <CountBox @input="(value) => changeCount(value, item.goods_id, item.goods_sku_id)" :value="item.goods_num"></CountBox>
-          </span>
+      </div>
+
+      <div class="footer-fixed">
+        <div @click="toggleAllCheck" class="all-check">
+          <van-checkbox :value="isAllChecked"  icon-size="18"></van-checkbox>
+          全选
+        </div>
+
+        <div class="all-total">
+          <div class="price">
+            <span>合计：</span>
+            <span>¥ <i class="totalPrice">{{ selPrice }}</i></span>
+          </div>
+          <div v-if="!isEdit" class="goPay" :class="{ disabled: selCount === 0 }" >结算({{ selCount }})</div>
+          <div v-else @click="handleDel" class="delete" :class="{ disabled: selCount === 0 }" >删除</div>
         </div>
       </div>
     </div>
 
-    <div class="footer-fixed">
-      <div @click="toggleAllCheck" class="all-check">
-        <van-checkbox :value="isAllChecked"  icon-size="18"></van-checkbox>
-        全选
+    <div class="empty-cart" v-else>
+      <img src="@/assets/empty.png" alt="">
+      <div class="tips">
+        您的购物车是空的, 快去逛逛吧
       </div>
-
-      <div class="all-total">
-        <div class="price">
-          <span>合计：</span>
-          <span>¥ <i class="totalPrice">{{ selPrice }}</i></span>
-        </div>
-        <div v-if="!isEdit" class="goPay" :class="{ disabled: selCount === 0 }" >结算({{ selCount }})</div>
-        <div v-else @click="handleDel" class="delete" :class="{ disabled: selCount === 0 }" >删除</div>
-      </div>
+      <div class="btn" @click="$router.push('/')">去逛逛</div>
     </div>
+
   </div>
 </template>
 
@@ -61,11 +73,14 @@ export default {
   },
   computed: {
     ...mapState('cart', ['cartList']),
-    ...mapGetters('cart', ['cartTotal', 'selCartList', 'selCount', 'selPrice', 'isAllChecked'])
+    ...mapGetters('cart', ['cartTotal', 'selCartList', 'selCount', 'selPrice', 'isAllChecked']),
+    isLogin () {
+      return this.$store.getters.token
+    }
   },
   created () {
     // 必须是登录过的用户，才能用户购物车列表
-    if (this.$store.getters.token) {
+    if (this.isLogin) {
       this.$store.dispatch('cart/getCartAction')
     }
   },
@@ -237,5 +252,31 @@ export default {
     }
   }
 
+}
+
+.empty-cart {
+  padding: 80px 30px;
+  img {
+    width: 140px;
+    height: 92px;
+    display: block;
+    margin: 0 auto;
+  }
+  .tips {
+    text-align: center;
+    color: #666;
+    margin: 30px;
+  }
+  .btn {
+    width: 110px;
+    height: 32px;
+    line-height: 32px;
+    text-align: center;
+    background-color: #fa2c20;
+    border-radius: 16px;
+    color: #fff;
+    display: block;
+    margin: 0 auto;
+  }
 }
 </style>
