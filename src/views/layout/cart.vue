44735243c8eded1fc53285f3f1115c950571e79c
jepson <jiangpeng@itcast.cn>
2023-07-03 02:59:42 +0800

    购物车结算

--- src/views/layout/cart.vue
+++ src/views/layout/cart.vue
@@ -41,7 +41,7 @@
             <span>合计：</span>
             <span>¥ <i class="totalPrice">{{ selPrice }}</i></span>
           </div>
-          <div v-if="!isEdit" class="goPay" :class="{ disabled: selCount === 0 }" >结算({{ selCount }})</div>
+          <div v-if="!isEdit" class="goPay" :class="{ disabled: selCount === 0 }"  @click="goPay">结算({{ selCount }})</div>
           <div v-else @click="handleDel" class="delete" :class="{ disabled: selCount === 0 }" >删除</div>
         </div>
       </div>
@@ -104,6 +104,19 @@ export default {
       if (this.selCount === 0) return
       await this.$store.dispatch('cart/delSelect')
       this.isEdit = false
+    },
+    goPay () {
+      // 判断有没有选中商品
+      if (this.selCount > 0) {
+        // 有选中的 商品 才进行结算跳转
+        this.$router.push({
+          path: '/pay',
+          query: {
+            mode: 'cart',
+            cartIds: this.selCartList.map(item => item.id).join(',') // 'cartId,cartId,cartId'
+          }
+        })
+      }
     }
   },
   watch: {
--- src/views/pay/index.vue
+++ src/views/pay/index.vue
@@ -29,33 +29,33 @@
     </div>
 
     <!-- 订单明细 -->
-    <div class="pay-list">
+    <div class="pay-list" v-if="order.goodsList">
       <div class="list">
-        <div class="goods-item">
+        <div class="goods-item" v-for="item in order.goodsList" :key="item.goods_id">
             <div class="left">
-              <img src="http://cba.itlike.com/public/uploads/10001/20230321/8f505c6c437fc3d4b4310b57b1567544.jpg" alt="" />
+              <img :src="item.goods_image" alt="" />
             </div>
             <div class="right">
               <p class="tit text-ellipsis-2">
-                 三星手机 SAMSUNG Galaxy S23 8GB+256GB 超视觉夜拍系统 超清夜景 悠雾紫 5G手机 游戏拍照旗舰机s23
+                {{ item.goods_name }}
               </p>
               <p class="info">
-                <span class="count">x3</span>
-                <span class="price">¥9.99</span>
+                <span class="count">x{{ item.total_num }}</span>
+                <span class="price">¥{{ item.total_pay_price }}</span>
               </p>
             </div>
         </div>
       </div>
 
       <div class="flow-num-box">
-        <span>共 12 件商品，合计：</span>
-        <span class="money">￥1219.00</span>
+        <span>共 {{ order.orderTotalNum }} 件商品，合计：</span>
+        <span class="money">￥{{ order.orderTotalPrice }}</span>
       </div>
 
       <div class="pay-detail">
         <div class="pay-cell">
           <span>订单总金额：</span>
-          <span class="red">￥1219.00</span>
+          <span class="red">￥{{ order.orderTotalPrice }}</span>
         </div>
 
         <div class="pay-cell">
@@ -65,7 +65,7 @@
 
         <div class="pay-cell">
           <span>配送费用：</span>
-          <span v-if="false">请先选择配送地址</span>
+          <span v-if="!selectedAddress">请先选择配送地址</span>
           <span v-else class="red">+￥0.00</span>
         </div>
       </div>
@@ -74,7 +74,7 @@
       <div class="pay-way">
         <span class="tit">支付方式</span>
         <div class="pay-cell">
-          <span><van-icon name="balance-o" />余额支付（可用 ¥ 999919.00 元）</span>
+          <span><van-icon name="balance-o" />余额支付（可用 ¥ {{ personal.balance }} 元）</span>
           <!-- <span>请先选择配送地址</span> -->
           <span class="red"><van-icon name="passed" /></span>
         </div>
@@ -88,7 +88,7 @@
 
     <!-- 底部提交 -->
     <div class="footer-fixed">
-      <div class="left">实付款：<span>￥999919</span></div>
+      <div class="left">实付款：<span>￥{{ order.orderTotalPrice }}</span></div>
       <div class="tipsbtn">提交订单</div>
     </div>
   </div>
@@ -96,11 +96,14 @@
 
 <script>
 import { getAddressList } from '@/api/address'
+import { checkOrder } from '@/api/order'
 export default {
   name: 'PayIndex',
   data () {
     return {
-      addressList: []
+      addressList: [],
+      order: {},
+      personal: {}
     }
   },
   computed: {
@@ -111,16 +114,29 @@ export default {
     longAddress () {
       const region = this.selectedAddress.region
       return region.province + region.city + region.region + this.selectedAddress.detail
+    },
+    mode () {
+      return this.$route.query.mode
+    },
+    cartIds () {
+      return this.$route.query.cartIds
     }
   },
   created () {
     this.getAddressList()
+    this.getOrderList()
   },
   methods: {
     async getAddressList () {
       const { data: { list } } = await getAddressList()
       this.addressList = list
-      console.log(list)
+    },
+    async getOrderList () {
+      const { data: { order, personal } } = await checkOrder(this.mode, {
+        cartIds: this.cartIds
+      })
+      this.order = order
+      this.personal = personal
     }
   }
 }
