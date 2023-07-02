d2abd32d2946328f6d545d1592ce5aa0263aa989
jepson <jiangpeng@itcast.cn>
2023-07-03 02:18:12 +0800

    通用接口封装

---
+++ src/api/order.js
@@ -0,0 +1,16 @@
+import request from '@/api/request'
+
+// 订单结算确认
+// mode: cart    => obj { cartIds }
+// mode: buyNow  => obj { goodsId  goodsNum  goodsSkuId }
+export const checkOrder = (mode, obj) => {
+  return request.get('/checkout/order', {
+    params: {
+      mode, // cart buyNow
+      delivery: 10, // 10 快递配送 20 门店自提
+      couponId: 0, // 优惠券ID 传0 不使用优惠券
+      isUsePoints: 0, // 积分 传0 不使用积分
+      ...obj // 将传递过来的参数对象 动态展开
+    }
+  })
+}
