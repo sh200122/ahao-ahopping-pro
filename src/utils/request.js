76d8f14b8fff55db5b41abaf56cc5caa99e5be65
ahao <ahao20010222@gmail.com>
2024-06-20 11:15:13 +0800

    three

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { Toast } from 'vant'
 // 创建 axios 实例，将来对创建出来的实例，进行自定义配置
 // 好处：不会污染原始的 axios 实例
 const instance = axios.create({
-  baseURL: 'http://cba.itlike.com/public/index.php?s=/api/',
+  baseURL: 'http://smart-shop.itheima.net/index.php?s=/api',
   timeout: 5000
 })
 
